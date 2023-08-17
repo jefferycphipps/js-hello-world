f74332a3cc72a7db8d3ef7601bef29fc553b3db7
Jeffery Phipps <jefferyphipps@att.net>
2023-08-17 10:22:11 -0500

    now says Hello World

--- hello.js
+++ hello.js
@@ -4,18 +4,24 @@ Enter your code on line 7 to print 'Hello, World!'
 to the console (the panel on the right)
 */
 
-console.log("Hello");
-
+console.log("Hello! I am Jeffery Phipps.");
+console.log(17+2);
 /*  
 Next, PLAY! 
 Modify your code on line 7 to try to accomplish the tasks listed below:
 
    1. Change the message that is printed.
+   Hello to the console
    2. Figure out what the parentheses do. Will the code work without them?
+   It lets the program know that the stuff in the middle is the parameter for the method/function.
    3. Remove one or both quotation marks. Do we need to include both opening and closing quote marks? Is there a difference between using a single or a double quote (' vs. ")?
+   It did not change the program in this case. In other languages it might. 
    4. Remove the semi-colon, ;.
+   The semicolon did not make a difference. In other languages it might.
    5. Print a number. (Bonus: Print two numbers added together).
+   Done
    6. Print multiple messages one after the other.
+   Done
    7. Print two messages on the same line.
    8. Print a message that contains quote marks, such as Quoth the Raven "Nevermore".
    9. Other. You choose!
